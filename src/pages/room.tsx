19ed692b610c959dc02f88eb6b32a18fb9d8a888
glatztp <gabrielfellipeglatz@gmail.com>
2025-07-07 23:18:18 -0300

    initial commit

---
+++ src/pages/room.tsx
@@ -0,0 +1,16 @@
+import { Navigate, useParams } from "react-router-dom";
+
+type RoomParams = {
+  roomId: string
+}
+
+export function Room() {
+
+  const params = useParams<RoomParams>()
+
+  if (!params.roomId){
+    return <Navigate replace to="/"/>
+  }
+
+  return <h1>Room DetailS {JSON.stringify(params)}</h1>;
+}
